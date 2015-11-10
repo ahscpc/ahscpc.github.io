297e7c0c88db73dc580732a8e7402a0f8565f5e9
Andrew Hyatt <ahyattdev@icloud.com>
2015-11-10 10:47:40 -0800

    Added to the game

--- js/text-adventure.js
+++ js/text-adventure.js
@@ -1,3 +1,8 @@
+var reset = function() {
+    display.innerText = "";
+    game.play();
+};
+
 var game = {
     
     write: function(text) {
@@ -6,31 +11,62 @@ var game = {
     
     keyPress: function(key) {
         if (key.keyCode == "13") {
-            var text = new String(input.textContent);
-            input.textContent = "";
-            this.onEnter(text);
+            game.onEnter(input.value);
+            input.value = "";
         }
     },
     
     play: function() {
+        
+        this.write("You are in a dark room with two doors.");
         this.write("[1] Enter the door on the left\n[2] Enter the door on the right");
         this.onEnter = function(text) {
-            if (text.textContent == "1") {
+            if (text == "1") {
                 this.leftDoor();
-            } else if (text.textContent == "2") {
-                this.rightDoor();
+            } else if (text == "2") {
+                this.die("There was a trap door infront of the door that opened when you opened the door. You fall to your death.");
             }
         };
     },
     
     leftDoor: function() {
-        this.write("You live");
+        this.write("You find yourself in a graveyard. It is night and there is no moon.");
+        this.write("[1] Run out of it as fast as you can\n[2]Observe your surroundings");
+        this.onEnter = function(text) {
+            if (text == "1") {
+                this.escape();
+            } else if (text == "2") {
+                this.die("A witch casts a spell on you, instantly killing you.");
+            }
+        }
     },
     
-    rightDoor: function() {
-        this.write("You die");
+    escape: function() {
+        this.write("You just barely evade the 10 skeletons that saw you and chased after you.");
+        this.write("[1]Attack the skeletons\n[2]Continue hiding");
+        this.onEnter = function(text) {
+            if (text == "1") {
+                this.die("At 10 to 1, they easily overpower you.");
+            } else if (text == "2") {
+                this.hide();
+            }
+        }
+    },
+    
+    die: function(message) {
+        this.write(message);
+        this.write("Game Over");
+        setTimeout(reset, 2000);
+    },
+    
+    hide: function() {
+        
     }
 };
 
+
+
 input.onkeydown = game.keyPress;
 game.play();
+
+
