6935fc90b8db716d9a67f02321b56c1338786481
Andrew Hyatt <andythehyatt@gmail.com>
2015-11-09 17:19:00 -0800

    Added to the game
    It still is broken…

--- js/text-adventure.js
+++ js/text-adventure.js
@@ -1,5 +1,36 @@
-var screen = document.getElementsByClassName("console")[0];
-
-var write = function(text) {
-    screen.innerText += text + "\n";
+var game = {
+    
+    write: function(text) {
+        display.innerText += text + "\n";
+    },
+    
+    keyPress: function(key) {
+        if (key.keyCode == "13") {
+            var text = new String(input.textContent);
+            input.textContent = "";
+            this.onEnter(text);
+        }
+    },
+    
+    play: function() {
+        this.write("[1] Enter the door on the left\n[2] Enter the door on the right");
+        this.onEnter = function(text) {
+            if (text.textContent == "1") {
+                this.leftDoor();
+            } else if (text.textContent == "2") {
+                this.rightDoor();
+            }
+        };
+    },
+    
+    leftDoor: function() {
+        this.write("You live");
+    },
+    
+    rightDoor: function() {
+        this.write("You die");
+    }
 };
+
+input.onkeydown = game.keyPress;
+game.play();
